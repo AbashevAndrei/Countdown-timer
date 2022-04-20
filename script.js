e1b9c58d2cb729ad9da222b09c4266ac3bb4c6c0
Abashev Andrew <abashev77@gmail.com>
2022-04-20 21:50:28 +1000

    Добавил таймер обратного отсчета

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Указываем дату события, для которого нужно рассчитать прошедшее время
-let appointedTime = new Date(1977, 0, 15)
+let appointedDate = new Date(1977, 0, 15)
 // Текущая дата
 let today = new Date()
 // Первый день текущего года
@@ -13,48 +13,6 @@ let month = new Date().getMonth() + 1
 // Название месяца из его номера
 let monthName = new Date().toLocaleString('ru', { month: 'long' })
 let date = new Date().getDate() */
-
-// Расчет количества дней, прошедших с начала года
-function daysPassed(now, startOfCurrentYear) {
-  // Находим разницу в миллисекундах, прошедших с 1970 года между двумя датами и приводим результат к дням. Округляем в меньшую сторону, т.к. остаются еще часы, минуты и секунды текущего дня. 86400000 мс это 24 часа.
-  return Math.floor((now - startOfCurrentYear) / 86400000)
-}
-
-// Отображение времени, прошедшего от указанной даты
-function timeFromEvent() {
-  // document.createElement('div.selector')
-  let appointedTime = new Date(1977, 0, 15)
-  let now = Date.now()
-  let counterFrom = document.querySelector('.counterFrom')
-  // Находим разницу между датами в миллисекундах
-  let timeDifferenceInMilliseconds = now - appointedTime
-  // Количество секунд (неполная минута)
-  let displaySeconds = Math.floor((timeDifferenceInMilliseconds / 1000) % 60)
-  // Количество минут (неполный час)
-  let displayMinutes = Math.floor(
-    (timeDifferenceInMilliseconds / 1000 / 60) % 60
-  )
-  // Количество часов (неполные сутки)
-  let displayHours = Math.floor(
-    (timeDifferenceInMilliseconds / 1000 / 60 / 60) % 24
-  )
-  // Количество дней с начала текущего года
-  let displayDays = daysPassed(now, startOfCurrentYear)
-  // Количество прошедших лет
-  let displayYears = Math.floor(
-    timeDifferenceInMilliseconds / 1000 / 60 / 60 / 24 / 365
-  )
-
-  displaySeconds < 10 ? (displaySeconds = '0' + displaySeconds) : displaySeconds
-  displayMinutes < 10 ? (displayMinutes = '0' + displayMinutes) : displayMinutes
-  displayHours < 10 ? (displayHours = '0' + displayHours) : displayHours
-
-  counterFrom.textContent = `Со дня рождения прошло: ${displayYears} л. ${displayDays} дн. ${displayHours} : ${displayMinutes} : ${displaySeconds}`
-
-  setTimeout(timeFromEvent, 1000)
-}
-
-timeFromEvent()
 
 // Отображение текущего времени каждую секунду
 function clock() {
@@ -78,3 +36,74 @@ function clock() {
 }
 
 clock()
+
+// Расчет количества дней, прошедших с начала года
+function daysPassed(now, startOfCurrentYear) {
+  // Находим разницу в миллисекундах, прошедших с 1970 года между двумя датами и приводим результат к дням. Округляем в меньшую сторону, т.к. остаются еще часы, минуты и секунды текущего дня. 86400000 мс это 24 часа.
+  return Math.floor((now - startOfCurrentYear) / 86400000)
+}
+
+// Отображение времени, прошедшего от указанной даты
+function timeFromEvent() {
+  let appointedDate = new Date(1977, 0, 15)
+  let now = Date.now()
+
+  // Находим разницу между датами в миллисекундах
+  let timeDifferenceInMilliseconds = now - appointedDate
+  // Количество секунд (неполная минута)
+  let displaySeconds = Math.floor((timeDifferenceInMilliseconds / 1000) % 60)
+  // Количество минут (неполный час)
+  let displayMinutes = Math.floor(
+    (timeDifferenceInMilliseconds / 1000 / 60) % 60
+  )
+  // Количество часов (неполные сутки)
+  let displayHours = Math.floor(
+    (timeDifferenceInMilliseconds / 1000 / 60 / 60) % 24
+  )
+  // Количество дней с начала текущего года
+  let displayDays = daysPassed(now, startOfCurrentYear)
+  // Количество прошедших лет
+  let displayYears = Math.floor(
+    timeDifferenceInMilliseconds / 1000 / 60 / 60 / 24 / 365
+  )
+
+  displaySeconds < 10 ? (displaySeconds = '0' + displaySeconds) : displaySeconds
+  displayMinutes < 10 ? (displayMinutes = '0' + displayMinutes) : displayMinutes
+  displayHours < 10 ? (displayHours = '0' + displayHours) : displayHours
+
+  let divCounterFrom = document.querySelector('.counterFrom')
+  divCounterFrom.textContent = `Со дня рождения прошло: ${displayYears} л. ${displayDays} дн. ${displayHours} : ${displayMinutes} : ${displaySeconds}`
+
+  setTimeout(timeFromEvent, 1000)
+}
+
+timeFromEvent()
+
+function counterTo() {
+  let appointedDate = new Date(2023, 0, 15)
+  let now = Date.now()
+
+  let timeDifferenceInMilliseconds = appointedDate - now
+  let displaySeconds = Math.floor((timeDifferenceInMilliseconds / 1000) % 60)
+  // Количество минут (неполный час)
+  let displayMinutes = Math.floor(
+    (timeDifferenceInMilliseconds / 1000 / 60) % 60
+  )
+  // Количество часов (неполные сутки)
+  let displayHours = Math.floor(
+    (timeDifferenceInMilliseconds / 1000 / 60 / 60) % 24
+  )
+  // Количество дней с начала текущего года
+  let displayDays = Math.floor(timeDifferenceInMilliseconds / 86400000)
+
+  displaySeconds < 10 ? (displaySeconds = '0' + displaySeconds) : displaySeconds
+  displayMinutes < 10 ? (displayMinutes = '0' + displayMinutes) : displayMinutes
+  displayHours < 10 ? (displayHours = '0' + displayHours) : displayHours
+
+  let divCounterTo = document.querySelector('.counterTo')
+  divCounterTo.textContent = `До события осталось: ${displayDays} дн. ${displayHours} : ${displayMinutes} : ${displaySeconds}`
+
+  setTimeout(counterTo, 1000)
+}
+
+counterTo()
